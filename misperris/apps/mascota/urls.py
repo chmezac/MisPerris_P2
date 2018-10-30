ef0f28d7dffa3a13ce651cfa16bf052c6e5dee02
chmezac <ch.mezac@alumnos.duoc.cl>
2018-10-30 08:29:06 -0400

    Agrega editar y borrar a Mis Perris

--- misperris/apps/mascota/urls.py
+++ misperris/apps/mascota/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from apps.mascota.views import index, mascota_view, mascota_list
+from apps.mascota.views import index, mascota_view, mascota_list, mascota_edit, mascota_delete
 from django.shortcuts import render, redirect
 
 
@@ -9,4 +9,6 @@ urlpatterns = [
      path('', index, name='index'),
      path('nuevo/', mascota_view, name='mascota_crear'),
      path('listar/', mascota_list, name='mascota_listar'),
+     path('editar/<int:id_mascota>/', mascota_edit, name='mascota_editar'),
+     path('eliminar/<int:id_mascota>/', mascota_delete, name='mascota_eliminar'),
 ]
--- misperris/apps/mascota/views.py
+++ misperris/apps/mascota/views.py
@@ -8,6 +8,7 @@ from apps.mascota.models import Mascota
 def index(request):
     return render(request, 'mascota/index.html')
 
+#AGREGAR
 def mascota_view(request):
     if request.method == 'POST':
         form = MascotaForm(request.POST)
@@ -19,7 +20,31 @@ def mascota_view(request):
     return render(request, 'mascota/mascota_form.html', {'form':form})
 
 
+#LISTAR
 def mascota_list(request):
-    mascota = Mascota.objects.all()
+    mascota = Mascota.objects.all().order_by('id')
     contexto = {'mascota':mascota}
     return render(request, 'mascota/mascota_list.html', contexto)
+
+
+#MODIFICAR
+def mascota_edit(request, id_mascota):
+	mascota = Mascota.objects.get(id=id_mascota)
+	if request.method == 'GET':
+		form = MascotaForm(instance=mascota)
+	else:
+		form = MascotaForm(request.POST, instance=mascota)
+		if form.is_valid():
+			form.save()
+		return redirect('mascota:mascota_listar')
+	return render(request, 'mascota/mascota_form.html', {'form':form})
+
+
+#ELIMINAR
+def mascota_delete(request, id_mascota):
+	mascota = Mascota.objects.get(id=id_mascota)
+	if request.method == 'POST':
+		mascota.delete()
+		return redirect('mascota:mascota_listar')
+	return render(request, 'mascota/mascota_delete.html', {'mascota':mascota})
+
