c4dea8b014f2ef553b3725d6f8f0b2be285eb9bd
chmezac <ch.mezac@alumnos.duoc.cl>
2018-10-30 07:36:30 -0400

    Agrega listar a Mis Perris

--- misperris/apps/mascota/urls.py
+++ misperris/apps/mascota/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from apps.mascota.views import index, mascota_view#, mascota_list
+from apps.mascota.views import index, mascota_view, mascota_list
 from django.shortcuts import render, redirect
 
 
@@ -8,5 +8,5 @@ app_name = 'mascota'
 urlpatterns = [
      path('', index, name='index'),
      path('nuevo/', mascota_view, name='mascota_crear'),
-     #path('listar/', mascota_list, name='mascota_listar'),
+     path('listar/', mascota_list, name='mascota_listar'),
 ]
--- misperris/apps/mascota/views.py
+++ misperris/apps/mascota/views.py
@@ -9,17 +9,17 @@ def index(request):
     return render(request, 'mascota/index.html')
 
 def mascota_view(request):
-	if request.method == 'POST':
-		form = MascotaForm(request.POST)
-		if form.is_valid():
-			form.save()
-		return redirect('mascota:index')
-	else:
-		form = MascotaForm()
-	return render(request, 'mascota/mascota_form.html', {'form':form})
+    if request.method == 'POST':
+        form = MascotaForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('mascota:index')
+    else:
+        form = MascotaForm()
+    return render(request, 'mascota/mascota_form.html', {'form':form})
 
 
-#def mascota_list(request):
-#    mascota = Mascota.objects.all()
-#    contexto = {'mascota':,mascota}
-#    return render(request, 'mascota/mascota_list.html', contexto)
+def mascota_list(request):
+    mascota = Mascota.objects.all()
+    contexto = {'mascota':mascota}
+    return render(request, 'mascota/mascota_list.html', contexto)
